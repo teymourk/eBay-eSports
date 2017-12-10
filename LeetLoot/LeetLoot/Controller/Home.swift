f8f275b2dc3a01d5e2df85493c95f05c8af9865a
Kiarash Teymoury <teymourk@oregonstate.edu>
2017-12-10 15:55:53 -0800

    Initial Project

---
+++ LeetLoot/LeetLoot/Controller/Home.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LeetLoot
+//
+//  Created by Kiarash Teymoury on 12/10/17.
+//  Copyright © 2017 Kiarash Teymoury. All rights reserved.
+//
+
+import UIKit
+
+class Home: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = . yellow
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
