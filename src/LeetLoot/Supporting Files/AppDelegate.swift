b119e81ad45d22ce30f5e0cb510c58ea10d07e51
Meagan Olsen <meaganolsen@Meagans-MacBook-Air.local>
2017-12-21 16:13:43 -0800

    setting up firebase

--- src/LeetLoot/Supporting Files/AppDelegate.swift
+++ src/LeetLoot/Supporting Files/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = UINavigationController(rootViewController: Home())
-        
+        FirebaseApp.configure()
         return true
     }
 
---
+++ src/Pods/Target Support Files/Pods-LeetLoot/Pods-LeetLoot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LeetLoot : NSObject
+@end
+@implementation PodsDummy_Pods_LeetLoot
+@end
