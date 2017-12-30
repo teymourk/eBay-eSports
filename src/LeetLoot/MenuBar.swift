68c197e411fc9f151aa49228061ae7045728445c
Kiarash Teymoury <teymourk@oregonstate.edu>
2017-12-29 23:07:26 -0800

    Adds Menubar base shell

---
+++ src/LeetLoot/MenuBar.swift
@@ -0,0 +1,25 @@
+//
+//  Menubar.swift
+//  LeetLoot
+//
+//  Created by Kiarash Teymoury on 12/29/17.
+//  Copyright © 2017 Kiarash Teymoury. All rights reserved.
+//
+
+import UIKit
+
+class Menubar: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        
+    }
+}
