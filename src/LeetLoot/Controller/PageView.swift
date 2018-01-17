fc2df1beaf87d3a3cc0dc0f90a7b72093bb2c47b
Kiarash Teymoury <teymourk@oregonstate.edu>
2018-01-17 13:20:58 -0800

    Removes PagingViewController

--- src/LeetLoot/Controller/PageView.swift
+++ src/LeetLoot/Controller/PageView.swift
@@ -9,9 +9,7 @@
 import Foundation
 import UIKit
 
-class PageView: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate, UIScrollViewDelegate {
-    
-    var pages = [UIViewController]()
+class PageView: UIViewController,  UIScrollViewDelegate {
     
     var menuBar = { () -> MenuBar in
         let view = MenuBar()
@@ -19,61 +17,18 @@ class PageView: UIPageViewController, UIPageViewControllerDataSource, UIPageView
         return view
     }()
     
-    override init(transitionStyle style: UIPageViewControllerTransitionStyle, navigationOrientation: UIPageViewControllerNavigationOrientation, options: [String : Any]? = nil) {
-        super.init(transitionStyle: .scroll, navigationOrientation: navigationOrientation, options: nil)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.dataSource = self
-        self.delegate = self
-        let initialPage = 0
-        let page1 = Home()
-        let page2 = Browse()
         
         // set navigation bar
         navigationItem.title = "LeetLoot"
         navigationController?.navigationBar.isTranslucent = false;
         
-        // add the individual viewControllers to the pages array
-        self.pages.append(page1)
-        self.pages.append(page2)
-        setViewControllers([pages[initialPage]], direction: .forward, animated: true, completion: nil)
-        
         setupMenuBar()
         setupNavBar()
-        setupScrollViewDelegation()
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        if let viewControllerIndex = self.pages.index(of: viewController) {
-            if viewControllerIndex == 0 {
-                return nil
-            } else {
-                // go to previous page in array
-                return self.pages[viewControllerIndex - 1]
-            }
-        }
-        return nil
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        if let viewControllerIndex = self.pages.index(of: viewController) {
-            if viewControllerIndex < self.pages.count - 1 {
-                // go to next page in array
-                return self.pages[viewControllerIndex + 1]
-            }
-        }
-        return nil
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
-        //Moving Horizontal bar goes here.
         print(scrollView.contentOffset.x)
     }
     
@@ -88,15 +43,6 @@ class PageView: UIPageViewController, UIPageViewControllerDataSource, UIPageView
         let layout = UICollectionViewFlowLayout()
         let myPge = Browse_Game(collectionViewLayout: layout)
         navigationController?.pushViewController(myPge, animated: true)
-    }
-    
-    //Set scrollView Delegate
-    private func setupScrollViewDelegation() {
-        for view in view.subviews {
-            if let scrollView = view as? UIScrollView {
-                scrollView.delegate = self
-            }
-        }
     }
     
     private func setupMenuBar() {
