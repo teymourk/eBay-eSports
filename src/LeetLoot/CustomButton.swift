7f7a6a8deea4a2c353ca7f720e7d30c26d74c711
Kia Teymoury <teymourk@oregonstate.edu>
2018-01-26 18:32:46 -0800

    Refactors MenuBar

--- src/LeetLoot/CustomButton.swift
+++ src/LeetLoot/CustomButton.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class CustomButton: UIButton {
-    convenience init(title: String, imageName: String? = nil) {
+
+protocol m {}
+
+extension m where Self: UIButton {
+     init(title: Menu.Options, imageName: String? = nil) {
         self.init(frame: .zero)
         let image = imageName == nil ? nil : UIImage(named: imageName!)
-        setTitle(title, for: .normal)
+        setTitle(title.title, for: .normal)
         titleLabel?.font = .systemFont(ofSize: 15)
         setImage(image, for: .normal)
         setTitleColor(.darkText , for: .normal)
         translatesAutoresizingMaskIntoConstraints = false
     }
 }
+
+extension UIButton: m {}
