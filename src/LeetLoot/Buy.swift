42f1180152191a78a9c0a58be111c41912fb337c
Kia Teymoury <teymourk@oregonstate.edu>
2018-01-27 22:48:59 -0800

    Adds Buy/Filter Views

---
+++ src/LeetLoot/Buy.swift
@@ -0,0 +1,33 @@
+//
+//  Buy.swift
+//  LeetLoot
+//
+//  Created by Kiarash Teymoury on 1/27/18.
+//  Copyright © 2018 Kiarash Teymoury. All rights reserved.
+//
+
+import UIKit
+
+class Buy: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class Filter: UITableView {
+    
+    override init(frame: CGRect, style: UITableViewStyle) {
+        super.init(frame: frame, style: .plain)
+        backgroundColor = .blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- src/LeetLoot/Buy_Filter.swift
+++ src/LeetLoot/Buy_Filter.swift
@@ -13,50 +13,50 @@ final class Buy_Filter: NSObject {
     internal enum Option {
         case Filter, Details, None
     }
+    private var frame: CGRect {
+        return CGRect(x: edgeOffset, y: height, width: width - 40, height: cutomHeight)
+    }
     
-    lazy var view = { () -> UIView in
-        let view = UIView(frame: CGRect(x: edgeOffset, y: height, width: width - 40, height: cutomHeight))
+    private lazy var buyView = { () -> Buy in
+        let view = Buy(frame: frame)
             view.layer.cornerRadius = 5
-            view.backgroundColor = .white
         return view
     }()
     
-    lazy var fadeBackgroud = { ()-> UIView in
+    private lazy var filterView = { () -> Filter in
+        let view = Filter(frame: frame)
+            view.layer.cornerRadius = 5
+        return view
+    }()
+    
+    private lazy var fadeBackgroud = { ()-> UIView in
         let view = UIView(frame: Constants.kWindow.frame)
             view.backgroundColor = .black
             view.alpha = 0
         return view
     }()
     
-    let width = Constants.kWidth
-    let height = Constants.kHeight
-    let window = Constants.kWindow
-    let cutomHeight = Constants.kHeight * (3.5/5)
-    let edgeOffset: CGFloat = 10
+    private let (width, height, window, cutomHeight) = (Constants.kWidth,
+                                                        Constants.kHeight,
+                                                        Constants.kWindow,
+                                                        Constants.kHeight * (3.5/5))
+
+    private let edgeOffset: CGFloat = 10
     
     func openPageFor(_ Option: Option) {
-        setupView()
-        switch Option {
-        case .Details:
-            print("Details Page")
-        case .Filter:
-            print("Filter Page")
-        default: break
-        }
-        
+        Option == .Details ? setupViewFor(buyView) : setupViewFor(filterView)
     }
     
-    private func setupView() {
+    private func setupViewFor(_ view: UIView) {
         window.addSubview(fadeBackgroud)
         window.addSubview(view)
         
         UIView.animate(withDuration: 0.5,
                        delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                         
-                        let y = (self.height) - (self.cutomHeight) - 10 //Needs to be Safelayout constant
-                        self.view.frame = CGRect(x: self.edgeOffset, y: y, width: Constants.kWidth - (self.edgeOffset * 2) , height: self.cutomHeight)
-                        self.fadeBackgroud.alpha = 0.5
-                        
+            let y = (self.height) - (self.cutomHeight) - 10 //Needs to be Safelayout constant
+            view.frame = CGRect(x: self.edgeOffset, y: y, width: Constants.kWidth - (self.edgeOffset * 2) , height: self.cutomHeight)
+            self.fadeBackgroud.alpha = 0.5
         })
     }
 }
