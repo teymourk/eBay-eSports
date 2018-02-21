8f5315cc08d3bdd83a92115fc7821c4ae160030c
Kia Teymoury <teymourk@oregonstate.edu>
2018-02-20 18:34:08 -0800

    Removes FirebaseAuthUI

--- src/LeetLoot/Controller/Home.swift
+++ src/LeetLoot/Controller/Home.swift
@@ -9,34 +9,14 @@
 import Foundation
 import UIKit
 import Firebase
-import FirebaseAuthUI
 
-
-class Home: UICollectionViewController, FUIAuthDelegate {
+class Home: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let authUI = FUIAuth.defaultAuthUI()
-        // You need to adopt a FUIAuthDelegate protocol to receive callback
-        authUI?.delegate = self
-        let authViewController = authUI!.authViewController()
-        self.present(authViewController, animated: true, completion: nil)
         setupCollectionView()
     }
-     //handle = Auth.auth().addStateDidChangeListener { (auth, user) in
-    
-    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
-        // handle user and error as necessary
-        if let user = user {
-            print(user.displayName ?? "no name?")
-        }
-        if let error = error {
-            print(error)
-        }
-    }
-    /*func logoutUser(_ sender: AnyObject) {
-        try Auth.auth()!.signOut();
-    }*/
+
     private func setupCollectionView() {
         collectionView?.backgroundColor = .customGray
         collectionView?.registerCell(Featured_Events_Cell.self)
