647b80375a22ef882367e217ba5fc05df669d0f6
wsims <willy.simz@gmail.com>
2018-02-21 22:39:02 -0800

    Fixes formatting for nav bar title

--- src/LeetLoot/View/CustomNavbar.swift
+++ src/LeetLoot/View/CustomNavbar.swift
@@ -11,34 +11,20 @@ import UIKit
 class CustomNavbar: ParentView {
     
     private let titleImage = { () -> UIImageView in
-        let image = UIImageView(image: #imageLiteral(resourceName: "rupee"))
+        let image = UIImageView(image: #imageLiteral(resourceName: "EliteLootLogo"))
             image.clipsToBounds = true
             image.contentMode = .scaleAspectFit
             image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
     
-    private let title = { () -> UILabel in
-        let label = UILabel()
-            label.text = "ELITE LOOT"
-            label.textColor = .lightBlue
-            label.font = UIFont(name: "Futura-Bold", size: 24)
-            label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
     override func setupView() {
         addSubview(titleImage)
-        addSubview(title)
         
         NSLayoutConstraint.activate([
-            titleImage.heightAnchor.constraint(equalTo: heightAnchor),
+            titleImage.topAnchor.constraint(equalTo: topAnchor, constant: -17),
             titleImage.leadingAnchor.constraint(equalTo: leadingAnchor),
             titleImage.trailingAnchor.constraintLessThanOrEqualToSystemSpacingAfter(trailingAnchor, multiplier: 1/2),
-            
-            title.centerYAnchor.constraint(equalTo: titleImage.centerYAnchor),
-            title.leadingAnchor.constraint(equalTo: titleImage.trailingAnchor),
-            title.trailingAnchor.constraint(equalTo: trailingAnchor),
         ])
     }
 }
